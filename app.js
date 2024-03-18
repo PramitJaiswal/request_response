dd32c13f9779bf94e4960144e3f7d0e82bbdc38a
Pramit <pramitjaiswal268@gmail.com>
2024-03-18 19:51:22 +0530

    commit 1

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Parse the request URL
+  const url = req.url;
+
+  // Custom responses based on the URL
+  if (url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome home');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to my Node Js project');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
